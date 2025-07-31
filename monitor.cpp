193ccaede6ff1b45832ba5a3defd23ca7c851f3e
Monica Philo S <s.monicaphilo@in.bosch.com>
2025-07-31 12:13:51 +0530

    complexity reduction

--- monitor.cpp
+++ monitor.cpp
@@ -3,41 +3,51 @@
 #include <thread>
 #include <chrono>
 #include <iostream>
+#include <string>
 using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
 
 
 
 
-void alert_Animation() {
-  for (int i = 0; i < 6; i++) {
-    cout << "\r* " << flush;
-    sleep_for(seconds(1));
-    cout << "\r *" << flush;
-    sleep_for(seconds(1));
-  }
-}
-void display_Alert(char* message)
-{
-  cout << message << "\n";
-  alert_Animation();
+bool isTemperatureNormal(float temperature) {
+    return temperature >= 95 && temperature <= 102;
 }
 
-bool within_Normal_Range(float value, float min, float max) {
-  return value >= min && value <= max;
+bool isPulseRateNormal(float pulseRate) {
+    return pulseRate >= 60 && pulseRate <= 100;
+}
+
+bool isSpo2Normal(float spo2) {
+    return spo2 >= 90;
+}
+
+void showAlertAnimation() {
+    for (int i = 0; i < 6; i++) {
+        cout << "\r* " << flush;
+        sleep_for(seconds(1));
+        cout << "\r *" << flush;
+        sleep_for(seconds(1));
+    }
+}
+
+void displayAlert(const char* message) {
+    cout << message << "\n";
+    showAlertAnimation();
 }
 
 int vitalsOk(float temperature, float pulseRate, float spo2) {
-  if (!within_Normal_Range(temperature, 95, 102)) {
-    display_Alert("Temperature is out of range!");
-    return 0;
-  } else if (!within_Normal_Range(pulseRate, 60, 100)) {
-    display_Alert("Pulse Rate is out of range!");
-    return 0;
-  } else if (spo2 < 90) {
-    display_Alert("Oxygen Saturation is out of range!");
-    return 0;
-  }
-  return 1;
+    if (!isTemperatureNormal(temperature)) {
+        displayAlert("Temperature is critical!");
+        return 0;
+    }
+    if (!isPulseRateNormal(pulseRate)) {
+        displayAlert("Pulse Rate is critical!");
+        return 0;
+    }
+    if (!isSpo2Normal(spo2)) {
+        displayAlert("Oxygen Saturation critical!");
+        return 0;
+    }
+    return 1;
 }
-
     
